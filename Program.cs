1e1b8e25f5551062ae4cc2a497036e2cc6fe64da
Caspar Brenneisen <caspar.brenneisen@hotmail.ch>
2016-04-06 20:33:22 +0200

    Sourcecode added
    Added the sourcecode of the Filelist-tool.

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+class main
+{
+	static void Main(string[] args)
+	{
+		string filelistExist = filelist.createFilelistFile();
+		if (filelistExist != null)
+		{
+			filelist.writeFilelist();
+		}
+		else
+		{
+			styling.WriteErrorLine("Won't write filelist because list-file doesn't exist!");
+		}
+
+		Console.WriteLine("Press any key to complete.....");
+		Console.ReadLine();
+	}
+}
+
+class filelist
+{
+	internal static void writeFilelist()
+	{
+		List<string> files = new List<string>();
+
+		string basePath = getBasePath();
+		String[] allfiles = Directory.GetFiles(basePath, "*.*", SearchOption.AllDirectories);
+		
+		foreach ( var file in allfiles){
+			FileInfo info = new FileInfo(file);
+
+			files.Add(file);
+		 }
+
+		File.WriteAllLines(basePath + "\\configuration\\filelist.txt", files, Encoding.UTF8);
+	}
+	internal static string createFilelistFile()
+	{
+		string basePath = getBasePath();
+
+		if(File.Exists(basePath + "\\configuration\\filelist.txt"))
+		{
+			Console.WriteLine("Tool found filelist in {0}\\configuration\\filelist.txt", basePath);
+			styling.WriteSuccessLine("Recreating the file to secure the integraty!");
+
+			File.Delete(basePath + "\\configuration\\filelist.txt");
+			File.Create(basePath + "\\configuration\\filelist.txt").Close();
+
+			return basePath;
+		}
+		else if(Directory.Exists(basePath+"\\configuration\\"))
+		{
+			Console.WriteLine("Creating the file {0}\\configuration\\filelist.txt.", basePath);
+
+			File.Create(basePath+"\\configuration\\filelist.txt").Close();
+
+			return basePath;
+		}
+		else
+		{
+			styling.WriteErrorLine("It look like, you started the tool in a false directory!");
+
+			return null;
+		}
+	}
+	protected static string getBasePath()
+	{
+		string basePath = Directory.GetCurrentDirectory();
+		Console.WriteLine("Tool get Directory {0} as Basepath.", basePath);
+
+		return basePath;
+	}
+}
+
+class styling
+{
+	public static void WriteErrorLine(string value)
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine(value);
+		Console.ResetColor();
+	}
+	public static void WriteSuccessLine(string value)
+	{
+		Console.ForegroundColor = ConsoleColor.Green;
+		Console.WriteLine(value);
+		Console.ResetColor();
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("CreateFilelist")]
+[assembly: AssemblyDescription("A Tool to create a filelist of an directory.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("chronyms.ch")]
+[assembly: AssemblyProduct("CreateFilelist")]
+[assembly: AssemblyCopyright("Copyright ©  2016 by Chronyms")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("29343035-cd05-44a8-bcb1-f0c2991d6e45")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
